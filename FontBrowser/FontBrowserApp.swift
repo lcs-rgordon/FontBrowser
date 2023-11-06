c92512518e9d368af2c4b0a46f735993b09aed5c
Russell Gordon <rgordon@lcs.on.ca>
2023-11-06 06:40:16 -0500

    Initial Commit

---
+++ FontBrowser/FontBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontBrowserApp.swift
+//  FontBrowser
+//
+//  Created by Russell Gordon on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct FontBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
